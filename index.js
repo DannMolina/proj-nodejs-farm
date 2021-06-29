30fde49fedc75a11e74a04c2954b1a2a0c7ebadd
Dann Russell Molina <dannrussell.molina@fligno.com>
2021-06-29 23:26:51 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const http = require("http");
+
+////////////////////////////////////////////
+// SERVER
+////////////////////////////////////////////
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+// * port 8000 && host 127.0.0.1
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
