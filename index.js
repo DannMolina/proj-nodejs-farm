c21b7069a14633fab4d506243b57ac6b78196930
Dann Russell Molina <dannrussell.molina@fligno.com>
2021-06-29 23:48:44 +0800

    created basic routing

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ////////////////////////////////////////////
 // SERVER
 ////////////////////////////////////////////
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      header: "This is a node app",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
   res.end("Hello from the server!");
 });
 
